df202868b903bd53594efdf6cffefb5605d1c1c5
Ramiro <ramiivan59@gmail.com>
2023-05-03 15:28:07 -0300

    otro git

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@ function mostrarProductos() {
     sneakers.forEach(sneaker => {
         const div = document.createElement("div")
         div.classList.add("item")
-        div.innerHTML = `
+        div.innerHTML = `<div class="item"
         <img class="destacados" src="${sneaker.imagen}" alt="${sneaker.nombre}">
             <div class="info-product">
                 <h4>${sneaker.nombre}</h4>
                 <p class="price">$${sneaker.precio}</p>
                 <button class="producto-agregar" id="${sneaker.id}">Añadir al carrito</button>
-            
+            </div>
             </div> `
 
             contenedorSneakers.append(div)
@@ -37,26 +37,27 @@ function actualizarBotones() {
 }
 
 
+
 const carrito = []
 
 function agregarAlCarrito(e) {
-    const buttonId = e.currentTarget.id
-    const sneakerAgregado = sneakers.find(sneaker => sneaker.id === buttonId)
+    const idBoton = e.currentTarget.id
+    const sneakerAgregado = sneakers.find(sneaker=>sneaker.id == idBoton)
 
-    if (carrito.some(sneaker => sneaker.id === buttonId)){
-        const index = carrito.findIndex(sneaker => sneaker.id === buttonId)
+    if (carrito.some(sneaker => sneaker.id == idBoton)){
+        const index = carrito.findIndex(sneaker => sneaker.id == idBoton)
         carrito[index].cantidad++
     }else{
         sneakerAgregado.cantidad = 1
         carrito.push(sneakerAgregado)
-    }   
-    numeroSube()
+    }
+    console.log(carrito)
+    numerosube()
 
-    localStorage.setItem("nuevo-carrito", JSON.stringify(carrito))
-    
 }
 
-function numeroSube() {
-    let otroNumero = carrito.reduce((acc, sneaker) => acc + sneaker.cantidad, 0)
-    numero.innerText = otroNumero
+function numerosube() {
+    let number = carrito.reduce((acc, sneaker) => acc + sneaker.cantidad, 0)
+    numero.innerText = number
 }
+
